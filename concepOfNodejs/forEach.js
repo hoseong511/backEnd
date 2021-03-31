d0f8179ea736fbd7f27c76e6dbaf1c22f7146432
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-03-31 23:43:29 +0900

    update foreach 비동기x map_filter

---
+++ concepOfNodejs/forEach.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const arr = [1,2,3,4,5]
+const newArr = []
+
+arr.forEach(item => {
+  newArr.push(item)
+});
+console.log(newArr);
---
+++ concepOfNodejs/map_filter.js
@@ -0,0 +1,6 @@
+const a = [1,2,3]
+// const b = a.map(x => x +1)
+// console.log(b);
+
+const f = a.filter( x => x > 1)
+console.log(f);
