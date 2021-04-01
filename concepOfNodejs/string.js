74764a3db50c6c33ccb97e81510d9fcfc083d81e
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-04-01 22:33:23 +0900

    update string method

---
+++ concepOfNodejs/string.js
@@ -0,0 +1,16 @@
+// string, js는 유니코드를 지원함. 한글, 이모티콘 모두 인식
+
+let string = 'node.js 올인원 패키지'
+
+let isStartWith = string.startsWith('n');
+let isIncludes = string.includes(',');
+let isEndWith = string.endsWith('지');
+
+const check = () => {
+  if (isEndWith && isIncludes && isStartWith) {
+    return true
+  }
+  return false
+}
+
+console.log(check());
