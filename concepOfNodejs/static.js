ad6e3e05a0ebef7b384c23230f2b4b0005adaa49
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-04-05 21:03:46 +0900

    update static

---
+++ concepOfNodejs/static.js
@@ -0,0 +1,19 @@
+'use strict'
+
+class test {
+  constructor(){
+
+  }
+
+  fn () {
+
+  }
+
+  static call () {
+    console.log("hoho12");
+  }
+}
+
+// const Test = new test()  일반적으로 이런식으로 class를 호출함
+
+test.call() // static method가 호출되는 방식
