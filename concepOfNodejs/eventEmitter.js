f9140e3f0ca0431575c61b2211f2aee89bc2c9d5
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-04-05 21:48:12 +0900

    update eventEmit

---
+++ concepOfNodejs/eventEmitter.js
@@ -0,0 +1,12 @@
+// 
+'use strict'
+
+const EventEmitter = require('events')
+
+class ChatManager extends EventEmitter {}
+const chatManager = new ChatManager()
+chatManager.on('join', () => {
+  console.log("new user joined");
+})
+
+chatManager.emit("join")
