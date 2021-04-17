91c01f2fa33031e2c4dfa88b08a84837c9488cbb
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-04-17 21:18:40 +0900

    update 부분적인 컴포넌트 사용- 조건1

--- react/study/src/App.js
+++ react/study/src/App.js
@@ -1,17 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// 컴포넌트의 앞글자는 대문자.. 코드컨벤션임.
+const Loading = () => <div>Loading...</div>
 
-const Head = props => <h1>{props.title}</h1>
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      loading: false
+    }
+  }
 
-function App() {
-  return (    
-    <>
-      <Head title="this is a title" name="this is a name"></Head>
-      <Head title="this is a title" name="this is a name"></Head>
-    </> // 표현하고자 하는 데이터를 넘기고 싶을때 이용하자
-    // 2가지 이상의 컴포넌트를 전달할 때 wrapper를 이용!
-    // 아무것도 지정하지 않는 것을 flagment라 함.
-  );
+  // 조건문을 사용하지 않고 상태 이용하기
+  
+  render() {
+    const { loading } = this.state
+    if (loading) return <Loading />
+    return <Loading/>
+  } 
 }
 
 export default App;
