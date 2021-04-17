a0fd5a4931cbc64d0b1d7a4b945bb77ad057d26f
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-04-17 20:42:51 +0900

    udpate 간단한 표현

--- react/study/src/App.js
+++ react/study/src/App.js
@@ -1,11 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+const data = [
+  {
+    title: 'Node',
+    value: 0
+  },
+  {
+    title: 'React',
+    value: 1
+  },
+]
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+          {
+            data.map(_ => (
+              <>
+                <p key={_.value}>{_.title}, {_.value}</p> 
+              </>
+            ))
+          }
+        
         <p>
           amazing <code>src/App.js</code> and save to reload.
         </p>
