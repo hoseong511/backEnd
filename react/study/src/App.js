da0600fe7cd9e60048b563973d8b8ad509f6717e
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-04-17 20:56:47 +0900

    update JavaScript XML 이용과 컴포넌트를 사용하여 반복적으로 표현하는법

--- react/study/src/App.js
+++ react/study/src/App.js
@@ -1,43 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
-const data = [
-  {
-    title: 'Node',
-    value: 0
-  },
-  {
-    title: 'React',
-    value: 1
-  },
-]
+const Head = props => <h1>{props.title}</h1>
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          {
-            data.map(_ => (
-              <>
-                <p key={_.value}>{_.title}, {_.value}</p> 
-              </>
-            ))
-          }
-        
-        <p>
-          amazing <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (    
+    <>
+      <Head title="this is a title" name="this is a name"></Head>
+      <Head title="this is a title" name="this is a name"></Head>
+    </> // 표현하고자 하는 데이터를 넘기고 싶을때 이용하자
+    // 2가지 이상의 컴포넌트를 전달할 때 wrapper를 이용!
+    // 아무것도 지정하지 않는 것을 flagment라 함.
   );
 }
 
