100a26966e21e76b95c34e89a9061d81d18f2c3c
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-04-17 21:27:33 +0900

    update 부분적인 컴포넌트 사용 -조건2

--- react/study/src/App.js
+++ react/study/src/App.js
@@ -11,11 +11,22 @@ class App extends React.Component {
   }
 
   // 조건문을 사용하지 않고 상태 이용하기
-  
+  comment () {
+    const con = 1
+    const res = con > 0 ? true : false;
+    // const and = loading && (<div>loading...</div>) 
+  }
   render() {
     const { loading } = this.state
-    if (loading) return <Loading />
-    return <Loading/>
+    return (
+      <>
+        {
+           loading && <Loading /> // 노드 시간에 했던 내용임. 
+           // 하나의 조건을 충족시킬때 이용하는 방법 앞쪽이 True여야 뒤가 실행
+           // || 는 앞이 false여야 뒤를 실행 앞이 true이면 뒤 실행 x
+        }
+      </>
+    )
   } 
 }
 
