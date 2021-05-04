a2fbde83318dbadcb24a3c8e9f6682c3ea3cfc37
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-05-04 20:57:27 +0900

    update express2: html 서빙

--- zerocho-nodejs/express/app.js
+++ zerocho-nodejs/express/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path')
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
-  res.send('hello express');
-});
+  res.sendFile(path.join(__dirname, 'index.html')); // sendFile 하면 알아서 fs을 사용함 -> 라우터 요청이 발생할 때마다 새롭게 불러오는 역할
+}); // 이런식으로 간단하게 html을 서빙!   서버는 이론이 더 중요하다. -> 기반 지식이 중요하다.
 
 app.post('/', (req, res) => {
   res.send('hello express');
@@ -18,3 +19,5 @@ app.get('/about', (req, res) => {
 app.listen(app.get('port'), () => {
   console.log('익스프레스 서버 실행');
 });
+
+// npm ls express -> 패키지 확인: 프로젝트내에서 사용하고 있는 패키지를 확인할 수 있다.
