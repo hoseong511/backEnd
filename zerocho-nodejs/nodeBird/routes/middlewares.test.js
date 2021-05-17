e5c38609ab75a853e5abf93e8343253eb2239a7b
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-05-17 13:21:18 +0900

    update unit Test

--- zerocho-nodejs/nodeBird/routes/middlewares.test.js
+++ zerocho-nodejs/nodeBird/routes/middlewares.test.js
@@ -1,3 +1,50 @@
-test('1 + 1은 2입니다', () => {
-  expect(1 + 1).toEqual(2);
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares')
+// TDD와 test의 차이??
+
+describe('isLoggedIn', () => { // 가짜를 흉내내서 테스트, 모킹
+  const res = {
+    status: jest.fn(() => res),
+    send: jest.fn(),
+  };
+  const next = jest.fn();
+  test('로그인되어 있으면 isLoggedIn이 next를 호출해야 함', () => {
+    const req = {
+      isAuthenticated: jest.fn(() => true)
+    };
+    isLoggedIn(req, res, next)
+    expect(next).toBeCalledTimes(1);
+  })
+  test('로그인되어 있지 않으면 isLoggedIn이 에러', () => {
+    const req = {
+      isAuthenticated: jest.fn(() => false)
+    };
+    isLoggedIn(req, res, next)
+    expect(res.status).toBeCalledWith(403);
+    expect(res.send).toBeCalledWith('로그인 필요')
+  })
 })
+
+describe('isNotLoggedIn', () => {
+  const res = {
+    status: jest.fn(() => res),
+    send: jest.fn(),
+    redirect: jest.fn()
+  };
+  const next = jest.fn();
+  test('로그인되어 있으면 isNotLoggedIn이 에러를 호출해야 함', () => {
+    const req = {
+      isAuthenticated: jest.fn(() => true)
+    };
+    const message = encodeURIComponent('로그인한 상태입니다.');
+    isNotLoggedIn(req,res,next);
+    expect(res.redirect).toBeCalledWith(`/?error=${message}`);
+  })
+  test('로그인되어 있지 않으면 isNotLoggedIn이 next를 호출해야 함', () => {
+    const req = {
+      isAuthenticated: jest.fn(() => false)
+    };
+    isNotLoggedIn(req,res,next);
+    expect(next).toBeCalledTimes(1);
+  })
+})
+// 코드를 기획을 하는것에 의미가 있다. -> TDD (Test Driven Development)
