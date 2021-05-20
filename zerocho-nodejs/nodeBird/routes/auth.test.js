c58392e22e86de1b0c540cd5ed5d45a4d9856190
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-05-20 16:53:49 +0900

    Update 로그인 기능 통합테스트하기

--- zerocho-nodejs/nodeBird/routes/auth.test.js
+++ zerocho-nodejs/nodeBird/routes/auth.test.js
@@ -1,13 +1,13 @@
 const request = require('supertest');
 const { sequelize } = require('../models');
 const app = require('../app');
-const { beforeFindAfterExpandIncludeAll } = require('../models/user');
 
 beforeAll(async () => {
   await sequelize.sync();
 })
 
 describe('POST /join', () => {
+  
   test('로그인 안했으면 가입', async (done) => {
     request(app)
       .post('/auth/join')
@@ -21,6 +21,41 @@ describe('POST /join', () => {
   })
 })
 describe('POST /login', () => {
+  const agent = request.agent(app);
+  beforeEach((done) => {
+    agent
+      .post('/auth/login')
+      .send({
+        email: 'hoho@hoho',
+        password: '1234',
+      })
+      .end(done);
+  });
+
+  test('이미 로그인했으면 redirect /', (done) => {
+    const message = encodeURIComponent('로그인한 상태입니다.');
+    agent
+      .post('/auth/join')
+      .send({
+        email: 'hoho@hoho',
+        nick: 'hoho',
+        password: '1234',
+      })
+      .expect('Location', `/?error=${message}`)
+      .expect(302, done);
+  });
+
+  test('가입되지 않은 회원', async(done) => {
+    const message = encodeURIComponent('가입되지 않은 ID입니다.')
+    request(app)
+      .post('/auth/login')
+      .send({
+        email: 'hoho1@hoho',
+        password: '1234'
+      })
+      .expect('Location', `/?loginError=${message}`)
+      .expect(302, done);
+  })
   test('로그인 수행', async (done) => {
     request(app)
       .post('/auth/login')
@@ -30,11 +65,45 @@ describe('POST /login', () => {
       })
       .expect('Location', '/')
       .expect(302, done);
-  })
-})
-// describe('GET /logout', () => {
+  });
 
-// })
+  test('비밀번호 틀림', async (done) => {
+    const message = encodeURIComponent('비밀번호가 맞지 않습니다.');
+    request(app)
+      .post('/auth/login')
+      .send({
+        email: 'hoho@hoho',
+        password: '12345',
+      })
+      .expect('Location', `/?loginError=${message}`)
+      .expect(302, done);
+  });
+});
+
+describe('GET /logout', () => {
+  test('로그인 되어있지 않으면 403', async (done) => {
+    request(app)
+      .get('/auth/logout')
+      .expect(403, done);
+  });
+   const agent = request.agent(app);
+   beforeEach((done) => {
+     agent 
+      .post('/auth/login')
+      .send({
+          email: 'hoho@hoho',
+          password: '1234',
+      })
+      .end(done);
+   });
+
+   test('로그아웃 수행', async (done) => {
+     agent
+        .get('/auth/logout')
+        .expect('Location', '/')
+        .expect(302, done);
+   });
+});
 
 afterAll(async () => {
   await sequelize.sync({force: true})
